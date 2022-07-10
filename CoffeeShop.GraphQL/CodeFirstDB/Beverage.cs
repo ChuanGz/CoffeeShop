4fbdf7b9165e812a3c1fa7ca2a425d3a7bcf3403
ChuanGz <thanhnhan.lionus@gmail.com>
2022-07-10 18:35:07 +0700

    Add project files.

---
+++ CoffeeShop.GraphQL/CodeFirstDB/Beverage.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.GraphQL.CodeFirstDB
+{
+    public class Beverage
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        public string Catalog { get; set; }
+
+        public double BasePrice { get; set; }
+        public double SalesPrice { get; set; }
+    }
+}
---
+++ CoffeeShop.GraphQL/CodeFirstDB/CoffeeShopApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.GraphQL.CodeFirstDB
+{
+    public class CoffeeShopApplicationDbContext : DbContext
+    {
+        public CoffeeShopApplicationDbContext(DbContextOptions<CoffeeShopApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Beverage> Beverages { get; set; }
+    }
+}
