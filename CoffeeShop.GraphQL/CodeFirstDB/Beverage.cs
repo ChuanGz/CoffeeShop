5dd7bf91f6a682a8d4ba7accb327c2301e624257
ChuanGz <thanhnhan.lionus@gmail.com>
2022-07-11 09:37:29 +0700

    COF-1 CoffeeShop SQLite3 database EF

--- CoffeeShop.GraphQL/CodeFirstDB/Beverage.cs
+++ CoffeeShop.GraphQL/CodeFirstDB/Beverage.cs
@@ -4,14 +4,11 @@ namespace CoffeeShop.GraphQL.CodeFirstDB
 {
     public class Beverage
     {
-        public int Id { get; set; }
+        public int BeverageId { get; set; }
 
         [Required]
         [StringLength(200)]
         public string Name { get; set; }
-
-        [StringLength(200)]
-        public string Catalog { get; set; }
 
         public double BasePrice { get; set; }
         public double SalesPrice { get; set; }
---
+++ CoffeeShop.GraphQL/Startup.cs
@@ -0,0 +1,13 @@
+﻿using CoffeeShop.GraphQL.CodeFirstDB;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.GraphQL
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CoffeeShopApplicationDbContext>(options => options.UseSqlite("Data Source=CoffeeShop.db"));
+        }
+    }
+}
